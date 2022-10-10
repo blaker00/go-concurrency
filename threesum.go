af4090f90419a18b846a6c8a9dbf44c4fa829496
Blake Wolfson <blaker00@yahoo.com>
2022-10-10 15:52:32 -0400

    3sum

---
+++ threesum.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"sort"
+)
+
+func threesum(input []int) [][]int {
+	//sort
+	data := input
+	sort.Ints(data)
+
+	//3sum
+	left := 0
+	right := len(data) - 1
+	var final [][]int
+	if len(input) < 2 {
+		fmt.Println("invalid input")
+		os.Exit(1)
+	}
+	for left < right {
+		if data[left]+data[left+1]+data[right] < 0 {
+			left++
+		} else if data[left]+data[left+1]+data[right] > 0 {
+			right--
+		} else {
+			if reflect.DeepEqual(final, [][]int{{data[left], data[left+1], data[right]}}) == false {
+				final = append(final, []int{data[left], data[left+1], data[right]})
+			}
+			left++
+		}
+
+	}
+	return final
+}
+
+func main() {
+	input := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threesum(input))
+
+}
