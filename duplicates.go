d17b68f6dd73b32da6a8628f19bbc69b510f16ea
Blake Wolfson <blaker00@yahoo.com>
2022-10-11 11:24:13 -0400

    duplicates.go

---
+++ duplicates.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
+func duplicates(input []int) {
+	if len(input) < 3 {
+		fmt.Println("invalid length of numbers")
+		os.Exit(1)
+	}
+
+	data := input
+	sort.Ints(data)
+
+	var final []int
+	for i := 1; i < len(data); i++ {
+		if data[i] != data[i-1] {
+			final = append(final, data[i-1])
+		} else if len(data)-1 == i && data[i-1] == data[i] {
+			final = append(final, data[i])
+		}
+	}
+	fmt.Println(final)
+}
+
+func main() {
+
+	data := []int{6, 6, 1, 1, 1, 2, 3, 4, 5, 5, 1}
+	duplicates(data)
+
+}
